a04368eb2cf61b2ced0fe50b55bb71424b8bb861
mocusura12 <32814933+mocusura12@users.noreply.github.com>
2022-08-09 08:37:57 +0900

    초기 커밋.

---
+++ 01_DashBoard/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using _01_DashBoard.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _01_DashBoard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Student()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Student([Bind("Name", "Age")] Student model)
+        {
+            return View();
+        }
+    }
+}
